9e74b90cda1a017c9d9c02428b052804c8ebef8e
Yogev Zipori <yogevzipori@Yogevs-MacBook-Pro.local>
2022-01-20 17:36:02 -0800

    not done

--- index.js
+++ index.js
@@ -2,6 +2,55 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
+
+function moveCharacter() {
+
+if(direction==='west'){
+    x = x - 1
+}
+if (direction === 'north'){
+    y = y + 1
+}
+
+if(direction === 'east'){
+    x = x + 1
+}
+
+if(direction === 'south'){
+    y = y - 1
+}
+character.style.left = x +'px'
+character.style.bottom = y + 'px'
+
+}
+setInterval(moveCharacter, 1)
+direction = 'east' 
+
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
 move(character).to(100, 250)
 
 
@@ -13,3 +62,4 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
